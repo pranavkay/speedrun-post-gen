b5788151808af2df3a973592dabf28bcddd6bad0
Pranav Kay <140229452+pranavkay@users.noreply.github.com>
2026-01-17 14:05:02 +0530

    Build docs output for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   base: '/speedrun-post-gen/',
   plugins: [react()],
+  build: {
+    outDir: 'docs',
+    emptyOutDir: true,
+  },
 });
